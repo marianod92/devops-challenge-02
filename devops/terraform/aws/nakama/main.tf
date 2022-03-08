feecb331808e58ee15c0aad4813e743db96e5968
marianod92 <mariano.dimodugno@gmail.com>
2022-03-08 20:23:46 -0300

    disabled node-grooups

--- devops/terraform/aws/nakama/main.tf
+++ devops/terraform/aws/nakama/main.tf
@@ -63,17 +63,17 @@ module "eks_cluster" {
   context                                                   = module.this.context
 }
 
-module "eks_node_group" {
-  enabled           = var.enabled
-  source            = "cloudposse/eks-node-group/aws"
-  version           = "0.27.1"
-  subnet_ids        = module.subnets.private_subnet_ids
-  cluster_name      = module.eks_cluster.eks_cluster_id
-  instance_types    = var.instance_types
-  desired_size      = var.desired_size
-  min_size          = var.min_size
-  max_size          = var.max_size
-  kubernetes_labels = var.kubernetes_labels
-  module_depends_on = module.eks_cluster.kubernetes_config_map_id
-  context           = module.this.context
-}
+# module "eks_node_group" {
+#   enabled           = var.enabled
+#   source            = "cloudposse/eks-node-group/aws"
+#   version           = "0.27.1"
+#   subnet_ids        = module.subnets.private_subnet_ids
+#   cluster_name      = module.eks_cluster.eks_cluster_id
+#   instance_types    = var.instance_types
+#   desired_size      = var.desired_size
+#   min_size          = var.min_size
+#   max_size          = var.max_size
+#   kubernetes_labels = var.kubernetes_labels
+#   module_depends_on = module.eks_cluster.kubernetes_config_map_id
+#   context           = module.this.context
+# }
