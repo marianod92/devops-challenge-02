04d1984250c413bed5c8760493d1d0502c63e88c
marianod92 <mariano.dimodugno@gmail.com>
2022-03-10 18:18:29 -0300

    fix outputs

--- devops/terraform/aws/nakama/eks/outputs.tf
+++ devops/terraform/aws/nakama/eks/outputs.tf
@@ -5,5 +5,5 @@ output "eks_cluster_id" {
 
 output "eks_cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster"
-  value       = module.eks_cluster.arn
+  value       = module.eks_cluster.eks_cluster_arn
 }
