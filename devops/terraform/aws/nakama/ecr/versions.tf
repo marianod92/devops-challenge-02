5d994d7b304d2ffd6b0532efcd9175d722d55b60
marianod92 <mariano.dimodugno@gmail.com>
2022-03-10 01:57:38 -0300

    Fix versions && update image repo to ECR

---
+++ devops/terraform/aws/nakama/ecr/versions.tf
@@ -0,0 +1,39 @@
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.38, < 4.0.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.3"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.8.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 0.17.7"
+    }
+  }
+}
