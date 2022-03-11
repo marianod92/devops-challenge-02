fcba9824184699e4970c127809a4a089b8645630
marianod92 <mariano.dimodugno@gmail.com>
2022-03-11 03:35:32 -0300

    Update bash

--- devops/utils/wait_for_dns.sh
+++ devops/utils/wait_for_dns.sh
@@ -3,11 +3,11 @@ while true
 do
   LB_DNS=$(kubectl get svc nakama -n nakama -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
   URL=$(echo "http://${LB_DNS}:7451/#/status")
-  echo "$URL"
   STATUS=$(curl -s -o /dev/null -w '%{http_code}' $URL)
 
   if [ $STATUS -eq 200 ]; then
-    echo "Got 200! All done!"
+    echo "Got 200! All done!" 
+    echo -e "\e[1;32m"${URL}"\e[1;m"
     break
   else
     echo "Got $STATUS :( Not done yet..."
