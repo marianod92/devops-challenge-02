460df874488380cb21b03de68d57f456fd930ea8
marianod92 <mariano.dimodugno@gmail.com>
2022-03-13 19:40:18 -0300

    fmt fix

--- devops/terraform/aws/nakama/ecr/main.tf
+++ devops/terraform/aws/nakama/ecr/main.tf
@@ -3,11 +3,11 @@ locals {
 }
 
 module "ecr" {
-  source                   = "cloudposse/ecr/aws"
-  version                  = "v0.32.3"
-  name                     = "ecr"
-  encryption_configuration = var.encryption_configuration
-  image_names              = ["nakama"]
-  context                  = module.this.context
+  source                     = "cloudposse/ecr/aws"
+  version                    = "v0.32.3"
+  name                       = "ecr"
+  encryption_configuration   = var.encryption_configuration
+  image_names                = ["nakama"]
+  context                    = module.this.context
   principals_readonly_access = length(local.principals_readonly_access_arns) > 0 ? local.principals_readonly_access_arns : []
 }
