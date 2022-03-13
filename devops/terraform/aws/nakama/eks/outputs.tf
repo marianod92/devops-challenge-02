7652f4657a46e3d94e87729e215ce8ae35057f0e
marianod92 <mariano.dimodugno@gmail.com>
2022-03-13 19:56:50 -0300

    Add eks nodegroup iam role arn

--- devops/terraform/aws/nakama/eks/outputs.tf
+++ devops/terraform/aws/nakama/eks/outputs.tf
@@ -7,3 +7,8 @@ output "eks_cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster"
   value       = module.eks_cluster.eks_cluster_arn
 }
+
+output "eks_node_group_role_arn" {
+  description = "ARN of the worker nodes IAM role"
+  value       = module.eks_node_group.eks_node_group_role_arn
+}
