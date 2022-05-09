cfccbc866e0356596f58647d7bb5b7368236710f
marianod92 <mariano.dimodugno@gmail.com>
2022-05-08 21:15:36 -0300

    Add vpc code from examples/complete vpc

--- devops/terraform/aws/nakama/vpc-anton/outputs.tf
+++ devops/terraform/aws/nakama/vpc-anton/outputs.tf
@@ -1,27 +1,27 @@
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
 
-output "vpc_cidr" {
-  value = module.vpc.vpc_cidr_block
-}
+# output "vpc_cidr" {
+#   value = module.vpc.vpc_cidr_block
+# }
 
-output "vpc_default_security_group_id" {
-  value = module.vpc.vpc_default_security_group_id
-}
+# output "vpc_default_security_group_id" {
+#   value = module.vpc.vpc_default_security_group_id
+# }
 
-output "public_subnet_cidrs" {
-  value = module.subnets.public_subnet_cidrs
-}
+# output "public_subnet_cidrs" {
+#   value = module.subnets.public_subnet_cidrs
+# }
 
-output "private_subnet_cidrs" {
-  value = module.subnets.private_subnet_cidrs
-}
+# output "private_subnet_cidrs" {
+#   value = module.subnets.private_subnet_cidrs
+# }
 
-output "public_subnet_ids" {
-  value = module.subnets.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.subnets.public_subnet_ids
+# }
 
-output "private_subnet_ids" {
-  value = module.subnets.private_subnet_ids
-}
+# output "private_subnet_ids" {
+#   value = module.subnets.private_subnet_ids
+# }
--- devops/terraform/aws/nakama/vpc-anton/versions.tf
+++ devops/terraform/aws/nakama/vpc-anton/versions.tf
@@ -1,39 +1,10 @@
-
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.38, < 4.0.0"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 1.13"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 2.2.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.4.1"
-    }
-    utils = {
-      source  = "cloudposse/utils"
-      version = ">= 0.17.7"
+      version = ">= 3.63"
     }
   }
 }
